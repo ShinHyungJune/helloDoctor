33a897050c8934ca988043e348ad69d950c80e34
ShinHyungJune <ssa4141@naver.com>
2022-10-25 17:55:52 +0900

    로그인 ,회원가입 추가

--- js/common.js
+++ js/common.js
@@ -1,64 +1,7 @@
 $(document).ready(function(){
     setTimeout(function(){
-        var agent = navigator.userAgent.toLowerCase();
-
-        if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {
-            $("body").addClass("ie");
-        }
-
-        if(navigator.vendor.indexOf("Apple") != -1){
-            $("body").addClass("safari");
-        }
-
-        $(window).scroll(function(){
-            $(this).scrollTop() > 50
-                ? $(".header").addClass("active")
-                : $(".header").removeClass("active");
-        });
-
-        $(".header .black").click(function(){
-            $(".header .btn-menu").removeClass("active");
-            $(this).removeClass("active");
-            $(".header .right").removeClass("active");
-            $(".header .black").removeClass("active");
-        });
-
-        $(".header .btn-menu").click(function(){
-            $(this).hasClass("active") ? closeSidebar() : openSidebar();
-        });
-
-        $(".m-tabs-tab").click(function(){
-            $(".m-tabs-tab").removeClass("active");
-            $(this).addClass("active");
-
-            $(".m-tabs-content").removeClass("active");
-            $(".m-tabs-content").eq($(this).index()).addClass("active");
+        $(".m-input-select.type01 .fake").click(function(){
+            $(this).siblings(".options").toggle();
         })
-
-        function openSidebar(){
-            $(".header .btn-menu").addClass("active")
-            $(".header .right").addClass("active");
-            $(".header .black").addClass("active");
-        }
-
-        function closeSidebar(){
-            $(".header .btn-menu").removeClass("active")
-            $(".header .right").removeClass("active");
-            $(".header .black").removeClass("active");
-        }
-
-        $(".header .right > .navs > .nav > .navs > .nav").click(function(){
-            if($(window).width() < 1200){
-                closeSidebar();
-            }
-        });
-
-        $(".header .util").click(function(){
-            $(this).addClass("active");
-        });
-
-        $(".m-script-toggle").click(function(){
-            $($(this).attr("data-target")).toggle();
-        })
-    }, 1000);
+    }, 500);
 });
